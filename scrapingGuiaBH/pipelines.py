0a18426919fd7985c6da5e7267ff6708f1598632
Lincoln Pires <lincoln.ufop@gmail.com>
2018-09-17 07:35:09 -0300

    Pipeline

--- scrapingGuiaBH/pipelines.py
+++ scrapingGuiaBH/pipelines.py
@@ -5,7 +5,32 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from sqlalchemy.orm import sessionmaker
+from scrapingGuiaBH.models import db_connect, create_table, Itens
 
 class ScrapingguiabhPipeline(object):
+
+    def __init__(self):
+        """
+            Inicializa a conexao e sessionmaker
+            Cria a tabela stage
+        """
+        engine = db_connect()
+        create_table(engine)
+        self.Session = sessionmaker(bind=engine)
+
     def process_item(self, item, spider):
+
+        session = self.Session()
+        stage = Itens(**item)
+
+        try:
+            session.add(stage)
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
         return item
