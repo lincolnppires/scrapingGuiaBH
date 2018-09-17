e2a5442cd23ca2205e53368d5111aa6fb58dd052
Lincoln Pires <lincoln.ufop@gmail.com>
2018-09-17 07:19:27 -0300

    Spider

--- scrapingGuiaBH/spiders/guia_bh_spider.py
+++ scrapingGuiaBH/spiders/guia_bh_spider.py
@@ -2,10 +2,28 @@
 import scrapy
 
 
-class GuiaBhSpiderSpider(scrapy.Spider):
+class Guia_bh_spider(scrapy.Spider):
     name = 'guia_bh_spider'
-    allowed_domains = ['guiabh.com.br/shows']
     start_urls = ['http://guiabh.com.br/shows/']
 
     def parse(self, response):
-        pass
+
+        yield {
+            "url": response.url,
+            "categoria": response.url.split("/")[3],
+            "descricao_evento": response.css('div.col-md-7>h1::text').extract_first(),
+            "local_evento": response.xpath('//div[@class=\'col-md-12\']').re(
+                r'(?:(?i)ENDEREÇO|LOCAL[\w,\W,\s,\d]*?[^</,<p>,\w])(?:[\w,\W,\s,\d]*?[\n,/b>])([\w,\W,\s,\d]*?</)'),
+
+            "local_venda": response.xpath('//div[@class=\'col-md-12\']').re(r'VENDA|INGRESSO[\w,\W]*'),
+            "tipo_ingresso": response.xpath('//div[@class=\'col-md-12\']').re(r'INGRESSO[\w,\W]*'),
+            "valor_ingresso": response.xpath('//div[@class=\'col-md-12\']').re(r'INGRESSO[\w,\W]*'),
+            "data_evento": response.xpath('//div[@class=\'col-md-7\']/span[@class=\'data\']').extract_first(),
+        }
+
+        proximos_links = response.xpath('//a/@href').re(r'(?:/shows/)(.*)')
+        for proximo in proximos_links:
+            if proximo is not None:
+                proximo = response.urljoin(proximo)
+                yield scrapy.Request(proximo, callback=self.parse)
+
